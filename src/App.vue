c2f9e88f0fbdf71133a88d777ae821cabfa751c1
Julio Cesar Torres Cama <julio.torres@ext.blaze.me>
2020-07-11 01:11:29 -0500

    request

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,46 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
-</template>
 
-<script lang="ts">
+<script lang="tsx">
 import Vue from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
+import lang from 'element-ui/lib/locale/lang/en.js'
+import locale from 'element-ui/lib/locale'
+import {Aside,Container,Select, Menu, Divider, Button} from "element-ui"
+
+locale.use(lang)
 
 export default Vue.extend({
   name: "App",
-  components: {
-    HelloWorld
+  render: h => {
+   /* 
+    <Aside>
+      <Menu>
+      <Select></Select>
+      </Menu>
+    </Aside>
+    */
+   /*
+   h('Aside',
+     {
+       class:{container:true},
+       style:{
+         background: 'green',
+        //  width: '100px'
+       }
+      //  attrs:{width:'10px'}
+     },
+     ['hellow']
+   )
+   */
+  return (<Aside
+  data={{class:{container:true}}}
+  // {..{data}}
+  
+  // data={{class:{container:true},style:{background:'green'}}}
+  // className="container"
+  // propsData={{class:{container:true}}}
+   >
+   render
+   </Aside>)
   }
 });
 </script>
@@ -25,5 +53,10 @@ export default Vue.extend({
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.container {
+  width: 400px;
+  background-color: #78a1ca;
+
 }
 </style>
